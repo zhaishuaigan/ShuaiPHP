c3e9aaff3ce3938f256785a48bad83577808a398
µÔ˧¸É <zhaishuaigan@qq.com>
2014-05-01 15:49:51 +0800

    添加文件结构

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header ( "Content-Type:text/html; charset=utf-8" );
-define ( 'SHUAI_PATH', './ShuaiPHP/' );
+define ( 'LIB_PATH', './ShuaiPHP/' );
 define ( 'APP_PATH', './App/' );
 define ( 'APP_DEBUG', true );
-require './ShuaiPHP/ShuaiPHP.php';
+require LIB_PATH . 'ShuaiPHP.php';

0298195b449fb0b653f947deb6a3d64bc566d63e
µÔ˧¸É <zhaishuaigan@qq.com>
2014-05-03 12:56:05 +0800

    关闭调试模式

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 header ( "Content-Type:text/html; charset=utf-8" );
 define ( 'LIB_PATH', './ShuaiPHP/' );
 define ( 'APP_PATH', './App/' );
-define ( 'APP_DEBUG', true );
+//define ( 'APP_DEBUG', true );
 require LIB_PATH . 'Start.php';
